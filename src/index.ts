7e1f22a7bf674494988976a36a4b51f65f93705e
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-03 17:53:48 +0300

    Updated CLI entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { program } from "commander";
-
+import { initRepository } from './commands/init';
 
 program.version("0.1.0").description("TrackIt - A Minimal git implementation in typescript")
+
+program
+  .command('init')
+  .description('Initialize a new repository')
+  .action(initRepository);
+
+program.parse(process.argv);
