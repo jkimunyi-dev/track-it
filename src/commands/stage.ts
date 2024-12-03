89c419322e050f31290b1c71902c79620d2ddeb3
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-03 18:00:20 +0300

    Created a StateManager to track state

--- src/commands/stage.ts
+++ src/commands/stage.ts
@@ -6,3 +6,16 @@ interface StagedFile {
   path: string;
   hash: string;
 }
+
+
+class StateManager{
+	private trackItPath: string;
+	private indexPath: string;
+	private objectsPath: string;
+
+	constructor(){
+		this.trackItPath = path.resolve(process.cwd(), ".track-it");
+		this.indexPath = path.join(this.trackItPath, "index");
+		this.objectsPath = path.join(this.trackItPath, "objects");
+	}
+}
