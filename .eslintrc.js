efecf59b5988794d6bab7dfcb334c1458f7ba46b
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-03 16:59:02 +0300

    ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: [
+	  'eslint:recommended',
+	  'plugin:@typescript-eslint/recommended',
+	  'prettier'
+	],
+	rules: {
+	  'prettier/prettier': 'error',
+	  '@typescript-eslint/explicit-function-return-type': 'error'
+	}
+  };
