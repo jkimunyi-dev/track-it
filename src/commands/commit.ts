57d7bc3f612e8042968cc9ea0329dfbd8de08df8
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-03 18:55:06 +0300

    CommitManager implementation

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -14,3 +14,18 @@ interface CommitObject{
 	parent?: string; // Optional reference to previous commit
 }
 
+class CommitManager{
+	private trackItPath: string;
+	private refsPath: string;
+	private objectsPath: string;
+	private indexPath: string;
+	private headPath: string;
+
+	constructor(){
+		this.trackItPath = path.resolve(process.cwd(), ".track-it");
+		this.refsPath = path.join(this.trackItPath, "refs");
+		this.objectsPath = path.join(this.trackItPath, "objects")
+		this.indexPath = path.join(this.trackItPath, "index");
+		this.headPath = path.join(this.trackItPath, "HEAD");
+	}
+}
