63424ee6e8ab72761b5541810824ab95cf732cfb
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-03 18:31:21 +0300

    Added stage to CLI entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { program } from "commander";
 import { initRepository } from './commands/init';
+import { stageCommand } from "./commands/stage";
 
 program.version("0.1.0").description("TrackIt - A Minimal git implementation in typescript")
 
@@ -7,5 +8,10 @@ program
   .command('init')
   .description('Initialize a new repository')
   .action(initRepository);
+
+program
+	.command("stage <files ...>")
+	.description("Stage files for commit")
+	.action(stageCommand)
 
 program.parse(process.argv);
