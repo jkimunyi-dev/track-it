a86d245129ce2b8d42645f89d6be4509edec62f6
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-05 23:32:33 +0300

    getLatestCommitHash method implementation

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -16,14 +16,35 @@ interface CommitObject{
 
 class LogManager{
 	private trackItPath: string
-	private refsPaths :string;
+	private refsPath :string;
 	private objectPath: string;
 	private headPath: string;
 
 	constructor(){
 		this.trackItPath = path.resolve(process.cwd(), ".track-it");
-		this.refsPaths = path.join(this.trackItPath, "refs")
+		this.refsPath = path.join(this.trackItPath, "refs")
 		this.objectPath = path.join(this.trackItPath, "objects");
 		this.headPath = path.join(this.trackItPath, "HEAD");
+	}
+
+	/**
+   * Get latest commit hash for the current branch
+   * @returns Commit hash or undefined if no commits exist
+   */
+	private getLatestCommitHash(): string | undefined {
+		const branchRefPath = path.join(this.refsPath, "heads", this.getCurrentBranch());
+		return fs.existsSync(branchRefPath) 
+		  ? fs.readFileSync(branchRefPath, "utf-8").trim() 
+		  : undefined;
+	  }
+	
+
+	/**
+	 * Display commit history starting from the latest commit
+	 * @param maxCommits Maximum number of commits to display (default : unlimited)
+	 */
+
+	public log(maxCommits? :number): void{
+		let currentCommit = getLatestCommitHash()
 	}
 }
